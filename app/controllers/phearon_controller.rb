120da72eb11e7cf4626d7944e0f2b05e0a4714ce
nicholastonelli <nicholasevantonelli@gmail.com>
2025-12-14 10:31:59 -0500

    base tailwind set

--- app/controllers/phearon_controller.rb
+++ app/controllers/phearon_controller.rb
@@ -1,7 +1,7 @@
 class PhearonController < ApplicationController
   #before_action :set_charactersheet, only: %i[ show edit update destroy ]
   def index
-
+    @characters = Character.all
   end
 
   def get
